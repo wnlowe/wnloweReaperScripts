c52c7419d17c3a070e7d85f70eb109dfa98839a2
wnlowe <williamnlowe@gmail.com>
2022-08-18 15:07:41 -0400

    Updated Formatting

--- [2] Completed/wnlowe_ClearRenderMatrix.lua
+++ [2] Completed/wnlowe_ClearRenderMatrix.lua
@@ -1,12 +1,9 @@
---Clear any regions rendering selected track
---by William N. Lowe
---wnlsounddesign.com
-
+-- Clear any regions rendering selected track
+-- by William N. Lowe
+-- wnlsounddesign.com
 ret, marks, regs = reaper.CountProjectMarkers(0)
 
-for ri = 0, regs do 
+for ri = 0, regs do
     reaper.SetRegionRenderMatrix(0, ri, reaper.GetSelectedTrack(0, 0), -1)
 end
-
-
 
