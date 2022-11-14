91cc1ab61c1b1ada58ccfba87af3f3e4bba67e76
wnlowe <43211247+wnlowe@users.noreply.github.com>
2022-11-14 13:20:05 -0500

    Bug squashing

--- [1] InProgress/wnlowe_MassHeadTails.lua
+++ [1] InProgress/wnlowe_MassHeadTails.lua
@@ -8,6 +8,7 @@ reaper.Main_OnCommand(40289, 0)
 
 for j = 0, numItems do
     reaper.SetMediaItemSelected( allItems[j], true )
-    reaper.Main_OnCommand(reaper.NamedCommandLookup("_RS9e12f1aa39697d84844c05ba90a19c5748408600", 0))
+    commandId = reaper.NamedCommandLookup("_RS9e12f1aa39697d84844c05ba90a19c5748408600")
+    reaper.Main_OnCommand(commandId, 0)
     reaper.SetMediaItemSelected( allItems[j], false )
 end
